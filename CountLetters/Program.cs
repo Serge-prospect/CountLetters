1e64dc684f526e2a3457f985cc4f731c55e3505f
Serge-prospect <prospect@tut.by>
2020-04-06 07:26:14 +0300

    1

---
+++ CountLetters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string input = Console.ReadLine();
+            int events = 0;                                                         // Successful events
+            int qty = 0;                                                            // Consecutive non-repeating letter quantity
+            int i = 0;                                                              // Current letter
+            int j = i+1;                                                            // Next letter
+            for (i = 0; i < input.Length && j < input.Length; i++, j++)
+            {                
+                if (input[i] != input[j])
+                    events++;                                                       // Count successful event
+            }
+            if (events > 0)
+                qty = events + 1;                                                   // Count letters 
+            Console.WriteLine($"Consecutive non-repeating letter quantity: {qty}");
+
+            Console.ReadKey();
+        }
+    }
+}
